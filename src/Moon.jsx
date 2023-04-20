3da5561b6498c0046037bbfd1e3f8ffd8ccb44a4
Erik Truckner <75428920+ErikTruckner@users.noreply.github.com>
2023-04-20 17:22:24 -0400

    Add files via upload

--- src/Moon.jsx
+++ src/Moon.jsx
@@ -8,8 +8,8 @@ const Moon = () => {
   const [moonTexture] = useTexture(['/assets/moon_map.jpg'])
 
   useFrame(({ clock }) => {
-    moonRef.current.position.x = Math.sin(clock.getElapsedTime() * 1) * 2
-    moonRef.current.position.z = Math.cos(clock.getElapsedTime() * 1) * 2
+    moonRef.current.position.x = Math.sin(clock.getElapsedTime() * 0.6) * 4
+    moonRef.current.position.z = Math.cos(clock.getElapsedTime() * 0.6) * 4
     moonRef.current.rotation.y += 0.005
   })
 
