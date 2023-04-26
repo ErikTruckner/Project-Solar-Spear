d39fd5f813fc7740e8eb28440119e7ce36103373
Erik Truckner <75428920+ErikTruckner@users.noreply.github.com>
2023-04-26 19:31:48 -0400

    Save point - onHover additions

--- src/planets/earth/Earth.jsx
+++ src/planets/earth/Earth.jsx
@@ -1,65 +1,17 @@
 import { useTexture } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import React, { useRef, useMemo, useCallback } from 'react'
+import React, { useRef, useState, useCallback } from 'react'
 import * as THREE from 'three'
 import Moon from './Moon'
 import ISS from './ISS'
-
-// const Earth = ({ displacementScale }) => {
-//   const earthRef = useRef()
-//   const earthPositionRef = useRef(new THREE.Vector3(10, 0, 0))
-
-//   const [
-//     earthTexture,
-//     earthNormalMap,
-//     earthSpecularMap,
-//     earthDisplacementMap,
-//     earthEmissiveMap,
-//   ] = useTexture([
-//     '/assets/earth_day.jpg',
-//     '/assets/earth_normal.jpg',
-//     '/assets/earth_specular.jpg',
-//     '/assets/earth_displacement.jpg',
-//     '/assets/earth_night.jpg',
-//   ])
-
-//   useFrame(({ clock }) => {
-//     // Calculate the Earth's position based on its angle from the Sun
-//     const angle = clock.getElapsedTime() * 0.5
-//     const distance = 10
-//     const x = Math.sin(angle) * distance
-//     const z = Math.cos(angle) * distance
-//     earthRef.current.position.set(x, 0, z)
-//     earthRef.current.rotation.y += 0.002
-//     earthPositionRef.current = earthRef.current.position
-//   })
-
-//   return (
-//     <group ref={earthRef}>
-//       <mesh castShadow receiveShadow>
-//         <sphereGeometry args={[1, 64, 64]} />
-//         <meshPhongMaterial
-//           map={earthTexture}
-//           normalMap={earthNormalMap}
-//           emissiveMap={earthEmissiveMap}
-//           emissive={0xffffff}
-//           emissiveIntensity={1.5}
-//           specularMap={earthSpecularMap}
-//           shininess={100}
-//           displacementMap={earthDisplacementMap}
-//           displacementScale={displacementScale}
-//         />
-//       </mesh>
-//       <Moon earthPosition={earthPositionRef.current} />
-//       <ISS earthPosition={earthPositionRef.current} />
-//     </group>
-//   )
-// }
 
 const Earth = React.memo(({ displacementScale }) => {
   const earthRef = useRef()
   const earthPositionRef = useRef(new THREE.Vector3(10, 0, 0))
   const clockRef = useRef(new THREE.Clock())
+
+  //
+  const [hovered, hover] = useState(false)
 
   const [
     earthTexture,
@@ -91,22 +43,31 @@ const Earth = React.memo(({ displacementScale }) => {
 
   return (
     <group ref={earthRef}>
-      <mesh castShadow receiveShadow>
+      <mesh
+        castShadow
+        receiveShadow
+        // NEW ADDITIONS
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+        // Use pointerEvents to change cursor on hover
+        onContextMenu={(event) => event.preventDefault()}
+        style={{ cursor: hovered ? 'pointer' : 'auto' }}>
+        {/*  */}
         <sphereGeometry args={[1, 64, 64]} />
         <meshPhongMaterial
           map={earthTexture}
           normalMap={earthNormalMap}
           emissiveMap={earthEmissiveMap}
           emissive={0xffffff}
-          emissiveIntensity={1.5}
+          emissiveIntensity={hovered ? 20 : 1.5}
           specularMap={earthSpecularMap}
           shininess={100}
           displacementMap={earthDisplacementMap}
           displacementScale={displacementScale}
         />
       </mesh>
-      <Moon earthPosition={earthPositionRef.current} />
-      <ISS earthPosition={earthPositionRef.current} />
+      <Moon />
+      <ISS />
     </group>
   )
 })
